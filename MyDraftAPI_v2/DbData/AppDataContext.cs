e89c9fc567089651d2c850684273b9e382d51b81
DFWTexan <ErishMF@gmail.com>
2023-05-06 20:39:41 -0500

    Add project files.

---
+++ MyDraftAPI_v2/DbData/AppDataContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MyDraftAPI_v2.DbData
+{
+    public class AppDataContext : DbContext
+    {
+        public AppDataContext()
+        {
+        }
+
+        public AppDataContext(DbContextOptions<AppDataContext> options) : base(options) { }
+
+    }
+}
