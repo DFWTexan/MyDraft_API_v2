3218b77c5797d5338b298e583baf5aaa82818061
DFWTexan <ErishMF@Gmail.com>
2023-05-08 11:20:11 -0500

    Update Enums.cs

--- MyDraftAPI_v2/DbData/DataModel/Enums/Enums.cs
+++ MyDraftAPI_v2/DbData/DataModel/Enums/Enums.cs
@@ -1,12 +1,12 @@
 ﻿namespace DataModel.Enums
 {
-    //public Enum Position
-    //{
-    //    Quarterback = 1,
-    //        RunningBack = 2,
-    //        WideReceiver = 3,
-    //        TightEnd = 4,
-    //        Kicker = 5,
-    //        Defense = 6
-    //}
+    public enum Position
+    {
+        Quarterback = 1,
+        RunningBack = 2,
+        WideReceiver = 3,
+        TightEnd = 4,
+        Kicker = 5,
+        Defense = 6
+    }
 }
