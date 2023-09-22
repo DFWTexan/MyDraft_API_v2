2c94fb9c0d3e98ef33e34bf72360c5df88c91e32
DFWTexan <ErishMF@Gmail.com>
2023-09-22 16:15:28 -0500

    Update DraftController.cs

--- MyDraftAPI_v2/Controllers/DraftController.cs
+++ MyDraftAPI_v2/Controllers/DraftController.cs
@@ -39,7 +39,7 @@ namespace MyDraftAPI_v2.Controllers
         ///     
         /// Get All Daft Picks for League
         ///
-        [HttpPost]
+        [HttpPut]
         public ActionResult GetDraftPicksForLeague([FromBody] ViewModel.ActiveLeague vInput)
         {
             //var result = new DataModel.Response.ReturnResult();
