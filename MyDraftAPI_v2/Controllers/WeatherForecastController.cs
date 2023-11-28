0a14fadea7f46fa4f247a51d02a0b952d1ad8f21
DFWTexan <ErishMF@Gmail.com>
2023-11-27 19:57:37 -0600

    Update WeatherForecastController.cs

--- MyDraftAPI_v2/Controllers/WeatherForecastController.cs
+++ MyDraftAPI_v2/Controllers/WeatherForecastController.cs
@@ -1,9 +1,11 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Net.Mail;
+using System.Net;
 
 namespace MyDraftAPI_v2.Controllers
 {
-    [Authorize]
+    //[Authorize]
     [ApiController]
     [Route("api/[controller]")]
     public class WeatherForecastController : ControllerBase
@@ -23,6 +25,27 @@ namespace MyDraftAPI_v2.Controllers
         [HttpGet(Name = "GetWeatherForecast")]
         public IEnumerable<WeatherForecast> Get()
         {
+            using (var client = new SmtpClient())
+            {
+                client.Host = "smtp.gmail.com";
+                client.Port = 587;
+                client.DeliveryMethod = SmtpDeliveryMethod.Network;
+                client.UseDefaultCredentials = false;
+                client.EnableSsl = true;
+                client.Credentials = new NetworkCredential("ErishMF@GMAIL.COM", "drfc cfap rzsx ohsf");
+                using (var message = new MailMessage(
+                    from: new MailAddress("ErishMF@GMAIL.COM", "TxNum5"),
+                    to: new MailAddress("EMFTest@mailinator.COM", "EMFTest-Email-Message")
+                    ))
+                {
+
+                    message.Subject = "Hello from code!";
+                    message.Body = "Loremn ipsum dolor sit amet ...";
+
+                    client.Send(message);
+                }
+            }
+
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateTime.Now.AddDays(index),
